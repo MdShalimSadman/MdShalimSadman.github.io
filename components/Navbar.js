1937dfb2f55ab278b5884f108b317faa8976cf2a
MdShalimSadman <shalimsadmandipro3@gmail.com>
2025-12-03 15:10:03 +0600

    feat(navbar): navbar fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,12 +5,16 @@ import Link from 'next/link'
 const Navbar = () => {
 
   return (
-    <header className="text-gray-600 body-font ">
-      <div className="px-8 container mx-auto flex flex-wrap  pb-5 flex-col md:flex-row items-center justify-between ">
+    <header className="text-gray-600 body-font md:fixed w-full !z-50 mt-6">
+      <div className="px-8 container mx-auto flex flex-wrap  pb-5 flex-col md:flex-row  justify-between ">
+        <div  className="bg-gray-900 !w-fit backdrop-blur-md  bg-opacity-50 rounded-lg">
         <a href={"/"} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <Image src="/logo3.png" alt="" width={180} height={40} />
+          <Image src="/logo3.png" alt="" width={120} height={120} />
         </a>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base text-white justify-center hidden md:block">
+        </div>
+
+         <div  className="bg-gray-900 backdrop-blur-md  bg-opacity-50 rounded-lg p-2 px-4 !h-fit hidden md:!block">
+        <nav className="md:ml-auto flex-wrap items-center text-sm text-white justify-center !flex !h-fit">
           <Link href="/#aboutMeRef" className="mr-5 hover:text-blue-500 font-bold text-lg">
             About me
           </Link>
@@ -23,11 +27,11 @@ const Navbar = () => {
           <Link href="/#institutesRef" className="mr-5 hover:text-blue-500 font-bold text-lg">
             Experience
           </Link>
-          <Link href="#contactRef" className="mr-5 hover:text-blue-500 font-bold text-lg">
+          <Link href="#contactRef" className=" hover:text-blue-500 font-bold text-lg">
             Contact
           </Link>
         </nav>
-
+</div>
 
       </div>
     </header>
